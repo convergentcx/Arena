e5c43491ce895afec5a04ebea991ec0c4efd7e73
Logan Saether <lsaether@protonmail.com>
2018-11-24 12:52:42 +0100

    amazingly works

--- src/pages/Leaderboard/index.jsx
+++ src/pages/Leaderboard/index.jsx
@@ -3,31 +3,93 @@ import { Table } from 'reactstrap';
 
 import withContext from '../../hoc/withContext';
 
+import PersonalEconomy from '../../build/contracts/PersonalEconomy.json';
+
+import { getMultihashFromBytes32 } from '../../Util';
+
+import ipfsApi from 'ipfs-api';
+
+const ipfs = ipfsApi('ipfs.infura.io', '5001', { protocol: 'https' });
+
 class LeaderboardList extends Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      events: [],
+      personalEconomies: [],
+      sub: null,
+    }
   }
 
-  async componentWillMount() {
-    console.log(this.props);
-    const { PersonalEconomyFactory } = this.props.drizzle.contracts;
+  componentDidMount() {
+    const { contracts: { PersonalEconomyFactory }, web3 } = this.props.drizzle;
 
-    PersonalEconomyFactory.events.Created({
+    const sub = PersonalEconomyFactory.events.Created({
       fromBlock: 0,
-    }).on('data', (event) => {
-      console.log(event);
-    })
+    }).on('data', async (event) => {
+      const { id } = event;
+      if (!this.state.events.find((event) => event.id === id)) {
+        // console.log('not found ' + id)
+        const personalEconomy = new web3.eth.Contract(PersonalEconomy.abi, event.returnValues.token_address);
+        // const name = await personalEconomy.methods.name().call();
+        const mhash = await personalEconomy.methods.mhash().call();
+        const contentAddress = getMultihashFromBytes32({
+          digest: mhash,
+          hashFunction: 18,
+          size: 32,
+        });
+        const raw = await ipfs.get(contentAddress);
+        const dataJson = JSON.parse(raw[0].content.toString());
+        const totalSupply = await personalEconomy.methods.totalSupply().call();
+        const newEconomy = {
+          name: dataJson.name,
+          services: dataJson.services,
+          totalSupply,
+        }
+        const list = [...this.state.personalEconomies, newEconomy];
+        this.setState({
+          personalEconomies: list,
+        });
+      } else {
+        // console.log('aleady has ' + id);
+      }
+    });
+
+    this.setState({
+      sub,
+    });
+  }
+
+  componentWillUnmount() {
+    this.state.sub.unsubscribe();
   }
 
   render() {
+    let tableRows = [];
+
+    const renderRows = async () => {
+      let i = 0;
+      while (i < this.state.personalEconomies.length) {
+        const personalEconomy = this.state.personalEconomies[i];
+
+        tableRows.push(
+          <tr key={i}>
+            <td>{personalEconomy.name}</td>
+            <td>{personalEconomy.totalSupply}</td>
+            <td>Dos</td>
+            <td>Tres</td>
+          </tr>
+        )
+        i++;
+      }
+    }
+
+    renderRows();
+
     return (
       <tbody>
-        <tr>
-          <td>Hello World</td>
-          <td>Hello World</td>
-          <td>Hello World</td>
-          <td>Hello World</td>
-        </tr>
+        {tableRows}
       </tbody>
     );
   }
