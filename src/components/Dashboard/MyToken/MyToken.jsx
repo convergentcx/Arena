23dff4c935284acd30d47253283090fd0223b290
acrdlph <achill@way.network>
2018-11-27 16:43:26 +0100

    all data (except services and prices) fetched for dashboard component

--- src/components/Dashboard/MyToken/MyToken.jsx
+++ src/components/Dashboard/MyToken/MyToken.jsx
@@ -14,6 +14,10 @@ import PersonalEconomy from '../../../build/contracts/PersonalEconomy.json';
 // import classes from '../ListToken/TokenDetails/TokenDetails.module.css';
 
 
+import CurveChart from './CurveChart/CurveChart'
+import BlockHistory from './BlockHistory/BlockHistory'
+
+
 import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
 import { withStyles } from '@material-ui/core/styles';
@@ -31,82 +35,201 @@ const styles = theme => ({
   },
 });
 
+const multiplier = 10 ** 18;
+
+
 class MyTokens extends Component {
-  componentDidMount() {
-    const { drizzle } = this.props;
+
+  state = {
+    dataKeys: {
+      totalSupplyKey: '',
+      yourBalanceKey: ''
+    },
+    exponent: 0,
+    inverseSlope: 0,
+    mhash: '',
+    name: '',
+    owner: '',
+    poolBalance: '',
+    symbol: '',
+    popover: false
+  };
+
+
+  async componentDidMount() {
+    const { address, drizzle } = this.props;
     const contractConfig = {
-      contractName: this.props.address,
-      web3Contract: new drizzle.web3.eth.Contract(PersonalEconomy['abi'], this.props.address)
+      contractName: address,
+      web3Contract: new drizzle.web3.eth.Contract(PersonalEconomy['abi'], address)
     };
     let drizzleEvents = ['Minted', 'Burned', 'Requested'];
-    drizzle.addContract(contractConfig, drizzleEvents);
+    await drizzle.addContract(contractConfig, drizzleEvents);
+
+    const contract = this.props.drizzle.contracts[address];
+
+    const totalSupplyKey = contract.methods.totalSupply.cacheCall();
+    const yourBalanceKey = contract.methods.balanceOf.cacheCall(
+      this.props.drizzleState.accounts[0]
+    );
+
+    const exponent = await contract.methods.exponent().call();
+    const inverseSlope = await contract.methods.inverseSlope().call();
+    const mhash = await contract.methods.mhash().call();
+    const owner = await contract.methods.owner().call();
+    const poolBalance = await contract.methods.poolBalance().call();
+
+    // TODO IPFS
+    const name = await contract.methods.name().call();
+    const symbol = await contract.methods.symbol().call();
+
+    this.setState({
+      dataKeys: {
+        totalSupplyKey,
+        yourBalanceKey
+      },
+      exponent,
+      inverseSlope,
+      mhash,
+      name,
+      owner,
+      poolBalance,
+      symbol
+    });
   }
 
   showDetails = () => {
     this.props.history.push('/tokens/' + this.props.address);
   };
 
+
+  getContractData = () => {
+
+  }
+
+
   render() {
     const { classes } = this.props;
 
-    // const rows = this.state.eventsArray && this.state.eventsArray.map(tokenEvents => (
-    //     <table>
-    //         {tokenEvents.map(event => (
-    //             <tr>
-    //                 <td>{event.event}</td>
-    //                 <td>{event.returnValues && (new Date(event.returnValues.time * 1000)).toDateString()}</td>
-    //                 <td>{event.returnValues && event.returnValues.message}</td>
-    //                 <td>{event.returnValues && event.returnValues.price}</td>
-    //             </tr>))}
-    //     </table>
+    const contract = this.props.drizzleState.contracts[this.props.address];
 
-    // ))
+    if (
+      !contract ||
+      !(this.state.dataKeys.yourBalanceKey in contract.balanceOf) ||
+      !(this.state.dataKeys.totalSupplyKey in contract.totalSupply)
+    ) {
+      return <div>Still Loading...</div>;
+    }
 
-    // const rows = this.state.eventsArray.map(event => {
-    //     console.log(event.returnValues && event.returnValues.time)
-    // })
+    const totalSupply = contract.totalSupply[this.state.dataKeys.totalSupplyKey].value;
+    const yourBalance = contract.balanceOf[this.state.dataKeys.yourBalanceKey].value;
+
+    const currentPrice =
+      (1 / this.state.inverseSlope) * (totalSupply / multiplier) ** this.state.exponent;
+
+
 
     return (
       <div id={this.props.address} className={classes.root}>
         <Grid container spacing={24}>
-          <Grid item xs={12}>
-  <Paper className={classes.paper} style={{fontWeight: 'bold', textAlign: 'left'}}>{this.props.name}
-                <div className={classes.DetailsButton}>
-                <Button color="secondary" size="sm" onClick={this.showDetails} style={{float: 'right'}}>
+          <Grid item xs={6}>
+            <Paper className={classes.paper} style={{ fontWeight: 'bold', textAlign: 'left' }}>{this.props.name}
+              <div className={classes.DetailsButton}>
+                <Button color="secondary" size="sm" onClick={this.showDetails} style={{ float: 'right' }}>
                   Details
             </Button>
               </div>
             </Paper>
           </Grid>
-          <Grid item xs={6} sm={3}>
-            <Paper className={classes.paper}>
-            <Money />
-            <h4>Some stats</h4>
+          <Grid item xs={3}>
+            <Paper className={classes.paper} style={{ fontWeight: 'bold', textAlign: 'left' }}>
+              <p> Market Cap </p>
+              <Money />
+              <h4>{currentPrice * (totalSupply / multiplier)}</h4>
+            </Paper>
+          </Grid>
+          <Grid item xs={3}>
+            <Paper className={classes.paper} style={{ fontWeight: 'bold', textAlign: 'left' }}>
+              <p>You hold</p>
+              <Money />
+              <h4>{yourBalance / multiplier.toFixed(3)}</h4>
+
             </Paper>
           </Grid>
           <Grid item xs={6} sm={3}>
             <Paper className={classes.paper}>
-            <Money />
-            <h4>Market cap</h4>            
+              <p>Current Price</p>
+              <Money />
+              <h4>{currentPrice}</h4>
             </Paper>
           </Grid>
           <Grid item xs={6} sm={3}>
             <Paper className={classes.paper}>
-            <Money />
-            <h4>Current Price</h4>    
+              <p>Current token supply</p>
+              <Money />
+              <h4>{totalSupply / multiplier}</h4>
             </Paper>
           </Grid>
           <Grid item xs={6} sm={3}>
             <Paper className={classes.paper}>
-            <Money />
-            <h4>Reserve</h4>                
+              <p>Reserve pool</p>
+              <Money />
+              <h4>{this.state.poolBalance / multiplier}</h4>
+            </Paper>
+          </Grid>
+          <Grid item xs={6} sm={3}>
+            <Paper className={classes.paper}>
+            <p>Bonding curve formula</p>
+              <Money />
+              <h4>{`p = 1 / ${this.state.inverseSlope} * x ^ ${this.state.exponent}`} </h4>
+            </Paper>
+          </Grid>
+
+          <Grid item xs={12} sm={6}>
+            <Paper className={classes.paper}>
+                <CurveChart
+                  curveData={{
+                    totalSupply: totalSupply/multiplier,
+                    poolBalance: this.state.poolBalance/multiplier,
+                    inverseSlope: this.state.inverseSlope,
+                    exponent: this.state.exponent,
+                    currentPrice: currentPrice
+                  }}
+                  margin={{
+                    top: 30,
+                    right: 10,
+                    bottom: 30,
+                    left: 10
+                  }}
+                  width={300}
+                  height={300}
+                />
             </Paper>
           </Grid>
           <Grid item xs={12} sm={6}>
-            <Paper className={classes.paper}>Some graphic (xs=12 sm=6)</Paper>
-          </Grid>
-          <Grid item xs={12} sm={6}>
-            <Paper className={classes.paper}>Some graphic (xs=12 sm=6)</Paper>
+            <Paper className={classes.paper}>
+            <CurveChart
+                  curveData={{
+                    totalSupply: 0,
+                    poolBalance: 0,
+                    inverseSlope: 0,
+                    exponent: 0,
+                    currentPrice: 0
+                  }}
+                  margin={{
+                    top: 30,
+                    right: 10,
+                    bottom: 30,
+                    left: 10
+                  }}
+                  width={300}
+                  height={300}
+                />
+            {/* 
+            Here I want to include a price chart, but I am not sure which props it needs and how to set up the contract so that 
+            the BlockHistory component can read the events out of it. The BlockHistory and PriceChart components are taken from Memelordz,
+            so we can look how it works there exactly.
+            <BlockHistory symbol={this.state.symbol} contract={this.props.drizzle.contracts[this.props.address]} showChart /> */}
+            </Paper>
           </Grid>
           <Grid item xs={12}>
             <Events date={this.props.date} address={this.props.address} />
@@ -114,10 +237,166 @@ class MyTokens extends Component {
 
 
         </Grid>
-
       </div>
     );
   }
 }
 
 export default withStyles(styles)(withContext(withRouter(MyTokens)));
+
+
+
+
+
+
+
+
+
+// class ProfileDetails extends Component {
+//   constructor(props) {
+//     super(props);
+//   }
+
+//   async componentDidMount() {
+//     const { addr, drizzle } = this.props;
+//     const contractConfig = {
+//       contractName: addr,
+//       web3Contract: new drizzle.web3.eth.Contract(PersonalEconomy.abi, addr)
+//     };
+//     const events = ['Minted', 'Burned'];
+//     await drizzle.addContract(contractConfig, events);
+
+//   }
+
+//   toggle = () => {
+//     this.setState({
+//       popover: !this.state.popover
+//     });
+//   };
+
+//   render() {
+
+//     return (
+//       <div style={{ padding: '5%' }}>
+
+//         <Grid container style={{ alignItems: 'center' }}>
+//           <IconButton onClick={() => this.props.history.goBack()}>
+//             <KeyboardBackspace />
+//           </IconButton>
+//           <div style={{ flexGrow: 1 }} />
+//           <Typography variant="title">
+//             Personal Economy of {this.props.addr}
+//           </Typography>
+//         </Grid>
+
+
+//         <Grid container style={{ paddingTop: '2%' }}>
+//           <BuySell
+//             contract={this.props.drizzle.contracts[this.props.addr]}
+//             drizzleState={this.props.drizzleState}
+//             symbol={this.state.symbol}
+//           />
+//           <Grid container style={{ paddingTop: '2%' }}>
+
+//             <Grid item md={6}>
+//               <ContractInfo
+//                 marketCap={currentPrice * (totalSupply / multiplier)}
+//                 name={this.state.name}
+//                 symbol={this.state.symbol}
+//                 tokenBalance={yourBalance / multiplier}
+//               />
+//             </Grid>
+
+//             <Grid item md={6}>
+//               <Card>
+//                 <div style={{ padding: '15px', }}>
+//                   Bonding Curve
+//                   <div>
+//                     <Button color="secondary" size="sm" id="Popover1" onClick={this.toggle}>
+//                       Details
+//                     </Button>
+//                   </div>
+//                   <Popover
+//                     placement="bottom"
+//                     isOpen={this.state.popover}
+//                     target="Popover1"
+//                     toggle={this.toggle}
+//                   >
+//                     <PopoverHeader>Contract Information</PopoverHeader>
+//                     <PopoverBody>
+//                       <Row>
+//                         <Col md={12}>
+//                           <FormGroup>
+//                             <Label size="sm" style={labelStyle}>
+//                               Contract Address
+//                             </Label>
+//                             <p> {this.props.addr} </p>
+//                           </FormGroup>
+//                         </Col>
+//                       </Row>
+//                       <Row>
+//                         <Col md={12}>
+//                           <Label size="sm" style={labelStyle}>
+//                             Owner Address
+//                           </Label>
+//                           <p>{this.state.owner}</p>
+//                         </Col>
+//                       </Row>
+//                       <Row>
+//                         <Col md={4}>
+//                           <Label size="sm" style={labelStyle}>
+//                             Price
+//                           </Label>
+//                           <p>{currentPrice} ETH</p>
+//                         </Col>
+//                         <Col md={4}>
+//                           <Label size="sm" style={labelStyle}>
+//                             Reserve Pool
+//                           </Label>
+//                           <p>{this.state.poolBalance} ETH </p>
+//                         </Col>
+//                         <Col md={4}>
+//                           <Label size="sm" style={labelStyle}>
+//                             Total Supply
+//                           </Label>
+//                           <p>
+//                             {totalSupply} {this.state.symbol}
+//                           </p>
+//                         </Col>
+//                       </Row>
+//                     </PopoverBody>
+//                   </Popover>
+//                 </div>
+
+//                 <CurveChart
+//                   curveData={{
+//                     totalSupply: totalSupply,
+//                     poolBalance: this.state.poolBalance,
+//                     inverseSlope: this.state.inverseSlope,
+//                     exponent: this.state.exponent,
+//                     currentPrice: currentPrice
+//                   }}
+//                   margin={{
+//                     top: 30,
+//                     right: 10,
+//                     bottom: 30,
+//                     left: 10
+//                   }}
+//                   width={300}
+//                   height={300}
+//                 />
+//               </Card>
+//             </Grid>
+//           </Grid>
+
+//           <RequestService
+//             account={this.props.drizzleState.accounts[0]}
+//             contract={this.props.drizzle.contracts[this.props.addr]}
+//             mhash={this.state.mhash}
+//             symbol={this.state.symbol}
+//           />
+//         </Grid>
+//       </div>
+//     );
+//   }
+// }
---
+++ src/components/Dashboard/MyToken/PriceChart/PriceChart.jsx
@@ -0,0 +1,153 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+
+function toNumber(num, dec) {
+    if (num === undefined || dec === undefined) return null;
+    // let n = new BN(num);
+    // dec = (10 ** dec).toString();
+    // let d = new BN(dec);
+    // return Number(n.div(d));
+    return num / (10 ** dec);
+  }
+const pad = (n) => n < 10 ? '0' + n : n;
+
+  
+const Recharts = require('recharts');
+
+const {
+  Area,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Line,
+  ReferenceDot,
+  ComposedChart
+} = Recharts;
+
+class PriceChart extends React.Component {
+  static contextTypes = {
+    contractActions: PropTypes.object,
+    contractParams: PropTypes.object,
+  }
+
+  state = {
+    priceInEth: true,
+  }
+
+  constructor(props) {
+    super(props);
+    this.getChartData = this.getChartData.bind(this);
+    this.formatTick = this.formatTick.bind(this);
+    this.toggle = this.toggle.bind(this);
+  }
+
+  componentDidMount() {
+    this.documentReady = true;
+    this.forceUpdate();
+  }
+
+  shouldComponentUpdate(nextProps, nextState) {
+    if (this.props.events !== nextProps.events) {
+      return true;
+    }
+    if (nextState.priceInEth !== this.state.priceInEth) {
+      return true;
+    }
+    return false;
+  }
+
+  getChartData(events) {
+    let { priceInEth } = this.state;
+    let value = 0;
+    let minTimestamp = events[0][1];
+    let values = events.map(([event, timestamp]) => {
+      switch (event.event) {
+        case 'Minted':
+          value += toNumber(priceInEth ? event.returnValues.totalCost : event.returnValues.amount, 18);
+          break;
+        case 'Burned':
+          value -= toNumber(priceInEth ? event.returnValues.reward : event.returnValues.amount, 18);
+          break;
+        default:
+          break;
+      }
+      return {
+        time: timestamp - minTimestamp,
+        when: new Date(timestamp * 1000).toString(),
+        value,
+      };
+    });
+    values.push({
+      time: Date.now() / 1000 - minTimestamp,
+      value,
+    });
+    return values;
+  }
+
+  formatTick(time) {
+    const minTimestamp = this.props.events[0][1];
+    time += minTimestamp;
+    time *= 1000;
+    const date = new Date(time);
+    if (!date) return '';
+    return date.getMonth() + '/' + pad(date.getDate(), 2) + ' ' + date.getHours() + ':' + pad(date.getMinutes(), 2);
+  }
+
+  toggle() {
+    this.setState({ priceInEth: !this.state.priceInEth });
+  }
+
+  render() {
+    if (!this.documentReady) return null;
+    let { events, currentPrice, symbol } = this.props;
+    if (!events || !events.length) return null;
+    let values = this.getChartData(events);
+    let price = values[values.length - 1];
+    let width = Math.min(600, (window.innerWidth < 480 ? window.innerWidth : 480) - 30);
+    let height = width * 2 / 3;
+    return (
+      <div >
+        <ComposedChart
+          style={{ margin: 'auto' }}
+          width={width}
+          height={height}
+          data={values}
+          margin={{ top: 10, right: 30, left: 0, bottom: 0 }}
+        >
+          <CartesianGrid strokeDasharray="3 3"/>
+          <XAxis
+            dataKey="time"
+            type="number"
+            tickFormatter={this.formatTick}
+            domain={['dataMin', 'dataMax']}
+          />
+          <YAxis
+            label={{ value: this.state.priceInEth ? 'ETH' : symbol, angle: -90, position: 'insideLeft' }} 
+            dataKey="value"
+            type='number'
+          />
+          <Area isAnimationActive={false} type="stepAfter" dataKey="value" stroke="blue" fill="blue" />
+          <ReferenceDot
+            isFront={true}
+            ifOverflow="extendDomain"
+            x={price.time}
+            y={price.value}
+            r={5}
+            fill="white"
+            stroke="blue"
+          />
+          <Tooltip />
+        </ComposedChart>
+        <div className='row'>
+          <button onClick={this.toggle}>
+            {this.state.priceInEth ? 'View ' + symbol + ' supply' : 'View prices in ETH'}
+          </button>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default PriceChart;
