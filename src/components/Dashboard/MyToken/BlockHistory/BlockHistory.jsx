0be307d79c6e2519705d5a49fbdddeb72e1163bf
Achill Rudolph <37867491+acrdlph@users.noreply.github.com>
2018-11-28 16:00:58 +0100

    Contract data fetched for Dashboard (#26)
    * all data (except services and prices) fetched for dashboard component
    
    * making it look nice
    
    * profile info card within dashboard taking shape
    
    * tabs added

---
+++ src/components/Dashboard/MyToken/BlockHistory/BlockHistory.jsx
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import PriceChart from '../PriceChart/PriceChart';
+
+class BlockHistory extends Component {
+  state = {
+    timestamps: {},
+    sort: [],
+  }
+
+  handleBlockLoad(block, event) {
+    if (block) {
+      this.setState({
+        timestamps: {
+          ...this.state.timestamps,
+          [event.blockHash]: parseInt(block.timestamp, 10),
+        }
+      });
+    }
+  }
+
+  render() {
+    const { contract, symbol, showChart, currentPrice } = this.props;
+    const { timestamps } = this.state;
+    const sortedEvents = contract.events
+      .map(event => [event, timestamps[event.blockHash] || Infinity])
+      .sort((a, b) => a[1] - b[1]);
+    return (
+      <div className='blockHistory'>
+        {showChart && <PriceChart events={sortedEvents} symbol={symbol} currentPrice={currentPrice} />}
+      </div>
+    );
+  }
+}
+
+export default BlockHistory;
